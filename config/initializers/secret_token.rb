a1270ff7702613525107e52dd5f8175dc99542e6
Russell Smith <russell.smith@ukd1.co.uk>
2013-12-18 10:34:48 -0800

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Readahead::Application.config.secret_key_base = 'c817853c9b1689be7342dea5749529dff960ab33b93103f96220e44bce8b82184a2d2af50d49c88b76ab395a832b49bf0a4b5e093e32acd37071f21efad5f43e'

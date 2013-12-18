ea787968ddf299d0b820a621949342e64e1c1acd
Robert Fruchtman <zuekn7yd@gmail.com>
2013-12-18 14:25:30 -0800

    Add Link::Representation class.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,19 @@
+require 'rest_client'
+
 class Link < ActiveRecord::Base
   validates :url, presence: true, url: true
+
+  def representation
+    @representation ||= Representation.new(url)
+  end
+
+  class Representation
+    attr_reader :data
+    delegate :title, :word_count, to: :data
+
+    def initialize(url)
+      raise "No Readability API key set" unless READABILITY_API_KEY
+      @data = RestClient.get "http://www.readability.com/api/content/v1/parser?url=#{url}&token=#{READABILITY_API_KEY}"
+    end
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -6,4 +6,16 @@ describe Link do
     it { should_not allow_value("www.google.com").for(:url) }
     it { should_not allow_value("not_a_url").for(:url) }
   end
+
+  describe Link::Representation do
+    let(:url) { 'https://news.ycombinator.com/' }
+
+    subject do
+      Link::Representation.new(url)
+    end
+
+    it 'should be available for a URL' do
+      expect{subject}.to_not raise_error
+    end
+  end
 end
